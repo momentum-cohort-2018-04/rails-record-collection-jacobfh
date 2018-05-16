9769d946d58aebcafa5f83f384ea0b3b74d1a566
Jacobfh <jfh708@gmail.com>
2018-05-16 15:05:02 -0400

    list index added

--- record-collection-app/app/controllers/albums_controller.rb
+++ record-collection-app/app/controllers/albums_controller.rb
@@ -2,7 +2,23 @@ class AlbumsController < ApplicationController
     def new
     end
 
+    def index
+    @albums = Album.all
+    end
+
     def create
-        render plain: params[:album].inspect
+        @album = Album.new(album_params)
+ 
+        @album.save
+        redirect_to @album
+    end
+
+    def show
+        @album = Album.find(params[:id])
+    end
+
+    private
+    def album_params
+        params.require(:album).permit(:title, :artist, :released)
     end
 end
